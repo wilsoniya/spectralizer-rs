2c1f4bfe2c02bc04d33ebc502d686ef1e7b0c5c3
Michael Wilson <wilsoniya@gmail.com>
2015-04-21 01:26:27 -0500

    first stab at pulse_simple FFI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#![feature(libc)]
+
+extern crate libc;
+use libc::{c_int, c_char, size_t};
+
+#[link(name = "pulse-simple")]
+#[link(name = "pulse")]
+extern {
+    fn pa_simple_new(server: *mut c_char, 
+                     name: *mut c_char,
+                     dir: c_int,
+                     dev: *mut c_char,
+                     steam_name: *mut c_char,
+                     sample_spec: *mut pa_sample_spec,
+                     channel_map: *mut u8,
+                     attr: *mut u8,
+                     error: *mut c_int) -> *mut pa_simple; 
+}
+
+// typedef struct pa_simple pa_simple 
+pub struct pa_simple;
+
+// see pulse/def.h
+pub struct pa_sample_spec {
+    format: c_int,
+    rate: u32,
+    channels: u8
+} 
+
 fn main() {
-    println!("Hello, world!");
+    let x = 10_i32;
+    println!("max compressed length of a 100 byte buffer: {}", x);
 }
